2eab87178f4d36a33080321d1b26bbc056bcda98
Seiryu1118 <116057980+Seiryu1118@users.noreply.github.com>
2023-01-12 10:48:33 +0000

    新規登録ログイン導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+   root to: 'public/homes#top'
+    get '/about' => 'public/homes#about'
+  # 会員用
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  #　管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+ namespace :public do
+    
+  end
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
